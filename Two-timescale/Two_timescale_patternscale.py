8a5244812990857ed5c1517535c898cac4a5a4bf
Jiacan Yuan <jy@vpn-client-172-16-9-192.rutgers.edu>
2019-12-14 09:20:52 +0800

    update code for two-timescale pattern scaling

--- Two-timescale/Two_timescale_patternscale.py
+++ Two-timescale/Two_timescale_patternscale.py
@@ -11,7 +11,7 @@ from statsmodels.formula.api import ols
 import pandas as pd
 import sys
 
-model='MPI-ESM-LR'
+model='bcc-csm1-1'
 # set the start and end years for the target period
 year_start = 1981
 year_end = 2100
@@ -27,7 +27,6 @@ T_45 = ds.T_45
 To_45 = ds.To_45
 T_85 = ds.T_85
 To_85 = ds.To_85
-
 
 # pre-processing zos data by removing global mean and smoothing by a 20-year moving average
 print('load zos data ...')
@@ -62,7 +61,7 @@ pvalues2 = np.zeros([2,nlat,nlon])
 
 gmt_sm = xr.concat((T_26.sel(years=slice(year_start, year_end)),T_45.sel(years=slice(year_start, year_end)),T_85.sel(years=slice(year_start, year_end))), dim='years')
 To = xr.concat((To_26.sel(years=slice(year_start, year_end)),To_45.sel(years=slice(year_start, year_end)),To_85.sel(years=slice(year_start, year_end))), dim='years')
-zos_sm = xr.concat((zos_anom_sm.sel(time=slice(year_start, year_end),scenarios=b'rcp26'),zos_anom_sm.sel(time=slice(year_start, year_end),scenarios=b'rcp45'),zos_anom_sm.sel(time=slice(year_start, year_end),scenarios=b'rcp85')), dim='time')
+zos_sm = xr.concat((zos_anom_sm.sel(time=slice(year_start, year_end),scenario=b'rcp26'),zos_anom_sm.sel(time=slice(year_start, year_end),scenario=b'rcp45'),zos_anom_sm.sel(time=slice(year_start, year_end),scenario=b'rcp85')), dim='time')
 for n in range(nlat):
     for m in range(nlon):
         temp1 = pd.DataFrame(np.column_stack([zos_sm[:,n,m],To, gmt_sm]), columns = ['zos','To','gmt'])
--- Two-timescale/preprocess_zos.py
+++ Two-timescale/preprocess_zos.py
@@ -42,15 +42,58 @@ def zos_preprocessing(model):
         lat_name = 'rlat'
         lon_name = 'rlon'
 
+    # historical
+    f1 = sorted(glob('/projects/kopp/jy519/data/CMIP5/monthly/{:}/zos_Omon_{:}_historical_r1i1p1_*.nc'.format(model.lower(), model)))
+    if len(f1)==1:
+        ds1 = xr.open_dataset(f1[0])
+        zosh = ds1.zos
+    else:
+        zosh_list = []
+        for j in range(len(f1)):
+            ds1 = xr.open_dataset(f1[j])
+            if model=='GISS-E2-R':
+                temp = ds1.zos
+                zos1=temp.where(temp!=0)
+            else:
+                zos1 = ds1.zos
+            zosh_list.append(zos1)
+        zosh = xr.concat(zosh_list, dim='time')
+
+    lat = ds1[lat_name]
+    lon = ds1[lon_name]
+    nlat = len(lat)
+    nlon = len(lon)
+    # compute the annual mean
+    yr_temp1 = zosh.time.dt.year
+    years1 = np.arange(yr_temp1.min(), 2006)
+    if zosh.time.dt.month[0]==12:
+        years1=years1[1:]
+    zosh_am = np.zeros([len(years1),nlat,nlon])
+    for i, year in enumerate(years1):
+        zosh_am[i,:,:] = zosh.sel(time=str(year)).mean(dim='time')
+    # remove global average at each time step
+    # convet the zos from np.array to data array
+    if (model == 'MPI-ESM-LR')|(model == 'IPSL-CM5A-LR'):
+        zos_da = xr.DataArray(zosh_am, coords=[years1, zosh.j, zosh.i], dims=['time', 'j', 'i'])
+        zosm = global_average_ij(zos_da, ds.lat)
+        zosh_anom = zos_da-zosm
+    elif (model == 'GISS-E2-R') | (model == 'HadGEM2-ES' )|(model == 'bcc-csm1-1'):
+        zos_da = xr.DataArray(zosh_am, coords=[years1, zosh[lat_name], zosh[lon_name]], dims=['time', 'lat', 'lon'])
+        zosm = global_average(zos_da)
+        zosh_anom = zos_da-zosm
+    # smooth the zos anomaly by a 20-yr moving average
+    zosh_sm = zosh_anom.rolling(time=20,center=True).mean()
+
     zos_anom_sm_list = []
+    # rcps
     for i, scen in enumerate(scenarios):
         print(scen)
-        f1 = sorted(glob('/projects/kopp/jy519/data/CMIP5/monthly/{:}/{:}/zos_Omon_{:}_{:}_r1i1p1_*.nc'.format(scen, model, model, scen)))
+        f1 = sorted(glob('/projects/kopp/jy519/data/CMIP5/monthly/{:}/zos_Omon_{:}_{:}_r1i1p1_*.nc'.format(model.lower(), model, scen)))
         if len(f1)==1:
             ds1 = xr.open_dataset(f1[0])
-            zosh = ds1.zos
+            zos = ds1.zos
         else:
-            zosh_list = []
+            zos_list = []
             for j in range(len(f1)):
                 ds1 = xr.open_dataset(f1[j])
                 if model=='GISS-E2-R':
@@ -58,37 +101,45 @@ def zos_preprocessing(model):
                     zos1=temp.where(temp!=0)
                 else:
                     zos1 = ds1.zos
-                zosh_list.append(zos1)
-            zosh = xr.concat(zosh_list, dim='time')
+                zos_list.append(zos1)
+            zos = xr.concat(zos_list, dim='time')
 
         lat = ds1[lat_name]
         lon = ds1[lon_name]
         nlat = len(lat)
         nlon = len(lon)
         # compute the annual mean
-        yr_temp1 = zosh.time.dt.year
-        years1 = np.arange(yr_temp1.min(), yr_temp1.max()+1)
-        if zosh.time.dt.month[0]==12:
-            years1=years1[1:]
-        zosh_am = np.zeros([len(years1),nlat,nlon])
-        for i, year in enumerate(years1):
-            zosh_am[i,:,:] = zosh.sel(time=str(year)).mean(dim='time')
+        try:
+            yr_temp1 = zos.time.dt.year
+            years1 = np.arange(yr_temp1.min(), yr_temp1.max()+1)
+            if zos.time.dt.month[0]==12:
+                years1=years1[1:]
+        except TypeError:
+            years1 = range(zos.time[0].dt.year, zos.time[-1].dt.year+1)
+        zos_am = np.zeros([len(years1),nlat,nlon])
+        for j, year in enumerate(years1):
+            if model=='bcc-csm1-1':
+                zos_am[j,:,:] = zos.sel(time=str(year).zfill(4)).mean(dim='time')
+            else:
+                zos_am[j,:,:] = zos.sel(time=str(year)).mean(dim='time')
 
 
         # remove global average at each time step
         # convet the zos from np.array to data array
         if (model == 'MPI-ESM-LR')|(model == 'IPSL-CM5A-LR'):
-            zos_da = xr.DataArray(zosh_am, coords=[years1, zosh.j, zosh.i], dims=['time', 'j', 'i'])
+            zos_da = xr.DataArray(zos_am, coords=[years1, zosh.j, zosh.i], dims=['time', 'j', 'i'])
             zosm = global_average_ij(zos_da, ds.lat)
             zos_anom = zos_da-zosm
         elif (model == 'GISS-E2-R') | (model == 'HadGEM2-ES' )|(model == 'bcc-csm1-1'):
-            zos_da = xr.DataArray(zosh_am, coords=[years1, zosh[lat_name], zosh[lon_name]], dims=['time', 'lat', 'lon'])
+            zos_da = xr.DataArray(zos_am, coords=[years1, zos[lat_name], zos[lon_name]], dims=['time', 'lat', 'lon'])
             zosm = global_average(zos_da)
             zos_anom = zos_da-zosm
 
         # smooth the zos anomaly by a 20-yr moving average
-        zos_sm = zos_anom.rolling(time=20,center=True).mean()
-        zos_anom_sm_list.append(zos_sm)
+        zosr_sm = zos_anom.rolling(time=20,center=True).mean()
+        zos_total = xr.concat((zosh_sm,zosr_sm),dim='time')
+        zos_anom_sm_list.append(zos_total)
+
     zos_anom_sm = xr.concat(zos_anom_sm_list, dim=pd.Index(scenarios, name='scenario'))
 
     return zos_anom_sm
